f3364ca4f772e5a7911ee93774dc62805f1dda31
zaydenSnacks <zaydenchin1@gmail.com>
2024-04-22 16:29:06 -0400

    added user auth

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+users = {'jabezdailey@icloud.com': 'password'}
+
+def auth(email, password):
+    if email in users and password == users[email]:
+        return True
+    else:
+        return False
+
 @app.route('/')
 def home():
-    return "Welcome to the Budgeting application";
+    return "Welcome to the Budgeting application"
+
+@app.route('/login', methods=['POST']) 
+def login():
+    email = request.form['email']
+    password = request.form['password']
+
+    if auth(email, password):
+        return "successful login!"
+    else:
+        return "Incorrect password or email."
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    email = request.form['email']
+    password = request.form['password']
+    users[email] = password
+    return "User created successfully!"
 
 if __name__ == '__main__':
     app.run(debug=True)
-
